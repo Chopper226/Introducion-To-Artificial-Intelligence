5a2d556287d7c8ce90eff373e7d304e1ce6ad62a
Chopper226 <layu.chen@gmail.com>
2023-04-05 00:27:00 +0800

    2D_k=5

---
+++ 非監督式學習 - k-Means/K = 5/2D.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+dx,dy = make_blobs( n_samples = 500 , n_features = 2 , centers = 10 , random_state = 42 )
+
+model = KMeans( n_clusters = 5 ) # > 分幾組
+model.fit( dx )
+new_dy = model.predict( dx )
+
+plt.figure( figsize = ( 10 , 10 ) )
+
+plt.subplot( 121 )
+plt.title( 'k-Means = 5 groups' )
+plt.scatter( dx.T[0] , dx.T[1] ,  c = new_dy , cmap = plt.cm.Set1 )
+
+plt.tight_layout()
+plt.show()
---
+++ 非監督式學習 - k-Means/K = 5/3D.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+dx , dy = make_blobs( n_samples = 500 , n_features = 3 , centers = 10 , random_state = 42 )
+
+model = KMeans( n_clusters = 5 ) # > 分幾組
+model.fit( dx )
+new_dy = model.predict( dx )
+
+fig = plt.figure( figsize = ( 16 , 8 ) )
+
+ax = fig.add_subplot( 121 , projection = '3d' )
+plt.title( 'KMeans =  5 groups' )
+ax.scatter( dx.T[0] , dx.T[1] , dx.T[2] , c = new_dy, cmap = plt.cm.Set1 )
+
+plt.show()
