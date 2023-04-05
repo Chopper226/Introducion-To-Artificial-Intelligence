7695506089e26844a243660d239f17b647ddcb2c
Chopper226 <layu.chen@gmail.com>
2023-04-05 15:42:50 +0800

    Decision tree

--- 監督式學習 - kNN 、 Decision Tree/Decision Tree.py
+++ 監督式學習 - kNN 、 Decision Tree/Decision Tree.py
@@ -21,3 +21,18 @@ DTree_test_score = DTree_model.score( x_test , y_test )
 print( 'predict : ', predict_DTree )
 print( 'real :',y_test ,'\n')
 print( DTree_test_score )
+
+
+'''
+Predict :
+
+X_test1 = [[1.51, 1.73, 1.98, 20.15, 85, 2.2, 1.92, .32, 1.48, 2.94, 1, 3.57, 172]]
+X_test2 = [[14.23, 1.71, 2.43, 15.6, 127, 2.8, 3.06, .28, 2.29, 5.64, 1.04, 3.92, 1065]]
+X_test3 = [[13.71, 5.65, 2.45, 20.5, 95, 1.68, .61, .52, 1.06, 7.7, .64, 1.74, 720]]
+
+print( "Decition Tree predict: ")
+print( "x_test1 : ", DTree_model.predict( X_test1 ))
+print( "x_test2 : ", DTree_model.predict( X_test2 ))
+print( "x_test3 : ", DTree_model.predict( X_test3 ))
+
+'''
